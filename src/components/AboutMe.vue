87bad70fcba97c6d64c9fcfbe0d2a23261cff993
Taco <wangziqi2002vip@163.com>
2024-04-18 22:46:53 -0500

    add

---
+++ src/components/AboutMe.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="container text-center">
+    <h1>
+      About Me
+    </h1>
+    <hr />
+    <About />
+  </div>
+</template>
+
+<script>
+const About = require('@/defaults/about.md')
+
+export default {
+  name: 'AboutMe',
+  components: {
+    About: About.vue.component
+  },
+  mounted () {
+    this.changeTitle('About')
+  }
+}
+</script>
---
+++ vue.config.js
@@ -0,0 +1,29 @@
+const Mode = require('frontmatter-markdown-loader/mode')
+const markdownIt = require('markdown-it')
+const markdownItHlJs = require('markdown-it-highlightjs')
+const markdownItLA = require('markdown-it-link-attributes')
+
+module.exports = {
+  publicPath: '/',
+  lintOnSave: false,
+  configureWebpack: {
+    module: {
+      rules: [
+        {
+          test: /\.md$/,
+          loader: 'frontmatter-markdown-loader',
+          options: {
+            mode: [Mode.VUE_COMPONENT],
+            markdownIt: markdownIt({ html: true })
+              .use(markdownItHlJs, { inline: true })
+              .use(markdownItLA, {
+                attrs: {
+                  target: '_blank'
+                }
+              })
+          }
+        }
+      ]
+    }
+  }
+}
